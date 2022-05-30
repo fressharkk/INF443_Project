f341c64468cfdd298a2f1dda39448957b3ec3fca
Raphabar <105667052+Raphabar@users.noreply.github.com>
2022-05-30 14:20:35 +0200

    Delete Raphael/projet_terrain directory

--- Raphael/projet_terrain/src/cart.hpp
+++
@@ -1,5 +0,0 @@
-
-
-#include "cgp/cgp.hpp"
-
-cgp::mesh create_cart_mesh(float terrain_length);
--- Raphael/projet_terrain/src/road.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "cgp/cgp.hpp"
-
-cgp::mesh create_road_mesh(float terrain_length);
-
-std::vector<cgp::vec3> generate_positions_on_road(int N, float terrain_length);
